d34f621ae5b11989b78e543176a963cb58f2ae0d
Benjamin Wende <benni.wende@gmail.com>
2023-02-12 12:05:51 +0100

    add OLED Bild.ino

---
+++ OLED Bild/OLED Bild.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Bild.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET -1 
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+void setup() {
+  Serial.begin(115200);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  display.clearDisplay();
+  display.drawXBitmap( 32 , 0 , bits , width , height , WHITE );
+  display.display();
+  
+}
+
+void loop() {
+
+}
